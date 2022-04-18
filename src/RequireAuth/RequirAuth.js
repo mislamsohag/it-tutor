6b01b649c7b457caec7c39b1e61ad5225de3a183
soahg <mislamsohag@gmail.com>
2022-04-18 16:31:57 +0600

    Some Files Update

--- src/RequireAuth/RequirAuth.js
+++ src/RequireAuth/RequirAuth.js
@@ -1,31 +1,24 @@
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import auth from './../Firebase/Firebase.init';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../Firebase/Firebase.init';
 
-const RequirAuth = ({ children }) => {
+// let auth = useAuth();
 
+
+const RequireAuth = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
-
-    // let auth = useAuth();
-
-    let location = useLocation();
+    const location = useLocation();
 
     if (loading) {
-        return <p>Loading ......................</p>
+        return <p>Loading</p>
     }
 
-    if (!user) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to when they were redirected. This allows us to send them
-        // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
-        return <Navigate to="/login" state={{ from: location }} replace />;
+    if (user) {
+        return children;
+    } else {
+        return <Navigate to="/login" state={{ from: location }} replace />
     }
-
-    return children;
-
-
 };
 
-export default RequirAuth;
+export default RequireAuth;
